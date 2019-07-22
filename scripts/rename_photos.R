86d65fb87b69f683a7cef2434265d629f2b2a983
Michael Belitz <michaelbelitz06@gmail.com>
2019-07-22 15:21:06 -0400

    test case of renaming photos

--- scripts/rename_photos.R
+++ scripts/rename_photos.R
@@ -1,106 +1,32 @@
 library(googlesheets)
 library(googledrive)
+library(dplyr)
 
-a <- gs_ls()
+a <- drive_ls(path = "Test Code")
 
 moth_id <- gs_title(x = "Moth Identification")
-
 moth_id_df <- gs_read(moth_id)
 
 rist_test <- moth_id_df %>% 
   dplyr::filter(Location == "RIST")
 
-auca_test <- moth_id_df %>% 
-  dplyr::filter(Location == "AUCA")
+rist_test_mini <- rist_test[1:3,]
 
-rist_test_baby_photo <- rist_test_baby$photoStart
-photonames <- paste(rist_test_baby_photo, ".JPG", sep = "")
+a <- a %>% 
+  mutate(photo_number = as.numeric(sub(".JPG*", "", sub("DSCN", "", name))))
 
-rename <- paste(rist_test_baby$Location, rist_test_baby$eventDate, sep = "")
-number_of_photos <- as.numeric(gsub(pattern = "DSCN", replacement = "", rist_test_baby$photoEnd)) - 
-  as.numeric(gsub(pattern = "DSCN", replacement = "", rist_test_baby$photoStart)) + 1
+rist_test_mini <- rist_test_mini %>% 
+  mutate(photoStart_number = as.numeric(sub(".JPG*", "", sub("DSCN", "", photoStart)))) %>% 
+  mutate(photoEnd_number = as.numeric(sub(".JPG*", "", sub("DSCN", "", photoEnd)))) %>% 
+  mutate(num_of_photos = photoEnd_number - photoStart_number + 1)
 
-rename2 <- paste(rename, rist_test_baby$macroCount, number_of_photos, sep = "_")
-rename2
-
-file <- drive_mv(bJPG, rename2)
-file <- as_dribble("Moth Phenology/Identification Photos/Copy of Copy of 26494738.JPG")
-file
-
-file <- drive_mv("Moth Phenology/Identification Photos/AUCA/Copy of Copy of 26494738.JPG", rename2)
-
-a <- as_dribble("Moth Phenology/Identification Photos/RIST/")
-a <- as_dribble("Moth Phenology/Identification Photos/RIST/DSCN5974.JPG")
-a
-
-dscn_count <- as.numeric(gsub(pattern = "DSCN", replacement = "", rist_test$photoEnd[1])) - 
-  as.numeric(gsub(pattern = "DSCN", replacement = "", rist_test$photoStart[1])) + 1
-dscn_count
-
-photos <- vector()
-for(i in 1:dscn_count) {
-  dscn_num <- as.numeric(gsub(pattern = "DSCN", replacement = "", rist_test$photoStart[1])) - 1
-  photos[i] <- paste("DSCN", dscn_num + i, ".JPG", sep = "")
-  
-}
-photos
-
-
-photo_list <- c()
-for(i in 1:nrow(rist_test_baby)){
-  number_of_photos <- as.numeric(gsub(pattern = "DSCN", replacement = "", rist_test_baby$photoEnd)) - 
-    as.numeric(gsub(pattern = "DSCN", replacement = "", rist_test_baby$photoStart)) + 1
-  
-  photo_vec <- c()
-  m <- matrix()
-  for(j in 1:number_of_photos[i]) {
-    dscn_num <- as.numeric(gsub(pattern = "DSCN", replacement = "", rist_test_baby$photoStart[i])) - 1
-    photo_vec[j] <- paste("DSCN", dscn_num + j, ".JPG", sep = "")
-  }
-  photo_list[[i]] <- photo_vec
-}
-photo_vec
-photo_list
-
-# try to do this within lists!
-lapplyfun <- function(x){as.numeric(sub(".JPG*","", sub("DSCN", "", x)))}
-photo_list_numeric <- lapply(photo_list, lapplyfun)
-photo_list_numeric
-
-rist_test_baby <- rist_test_baby %>% 
-  dplyr::mutate(photoStartNumeric = as.numeric(gsub(pattern = "DSCN", replacement = "", rist_test_baby$photoStart))) %>% 
-  dplyr::mutate(photoEndNumeric = as.numeric(gsub(pattern = "DSCN", replacement = "", rist_test_baby$photoEnd)))
-
-photo_list_numeric
-
-for(i in nrow(rist_test_baby)){
-  if(x)
-}
-
-# now let's rename
-
-photo_unlist <- unlist(photo_list)
-photo_unlist
-photo_unlist_numeric <- as.numeric(sub(".JPG*","", sub("DSCN", "", photo_unlist)))
-photo_unlist_numeric
-rename2
-photo_start_numeric <- as.numeric(gsub(pattern = "DSCN", replacement = "", rist_test_baby$photoStart))
-photo_end_numeric <- as.numeric(gsub(pattern = "DSCN", replacement = "", rist_test_baby$photoEnd))
-
-for(i in 1:length(photo_unlist_numeric)){
-  for(j in 1:nrow(rist_test_baby)){
-    for(k in 1:length(number_of_photos)){
-      if(photo_unlist_numeric[i] >= photo_start_numeric[j] & photo_unlist_numeric[i] <=photo_end_numeric[j]){
-      drive_mv(file = paste("Moth Phenology/Identification Photos/", paste(rist_test_baby$Location[j],"/",sep = ""),
-                            photo_unlist[i], sep = ""),
-              paste(rename[i], rist_test_baby$macroCount[j], k, sep = "_"))
-    } else
-      print("NADA")
-      
-  }
-}}
-  
-# almost there! ugh!
-
-rename2 <- paste(rename, rist_test_baby$macroCount, number_of_photos, sep = "_")
-photo_unlist_numeric %>% dplyr::between(photo_start_numeric[1], photo_end_numeric[1], )
+for(i in 1:nrow(a)){
+  for(j in 1:nrow(rist_test_mini)){  
+    ifelse(a$photo_number[i] >= rist_test_mini$photoStart_number[j] & 
+             a$photo_number[i] <= rist_test_mini$photoEnd_number[j], 
+           drive_mv(file = as_id(a$id[i]), paste(rist_test_mini$Location[j], 
+                                                 rist_test_mini$eventDate[j],
+                                                 a$photo_number[i], sep = "_")),
+           print("Skipping to next iteration"))
+    
+  }}
