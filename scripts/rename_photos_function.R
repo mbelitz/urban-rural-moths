0f7226e0a11f979f82814365bc960af45f6c2e47
Michael Belitz <michaelbelitz06@gmail.com>
2019-07-23 13:53:07 -0400

    used grepl as ifelse test

--- scripts/rename_photos_function.R
+++ scripts/rename_photos_function.R
@@ -3,27 +3,44 @@ library(googledrive)
 library(dplyr)
 library(tools)
 
-rename_photos <- function(drive_ls_path, moth_id_dataframe){
+rename_photos <- function(drive_ls_path, moth_id_dataframe, Location){
   
-  a <- drive_ls(path = drive_ls_path)
+  a <- drive_ls(path = drive_ls_path) 
   
   a <- a %>% 
-    mutate(photo_number = as.numeric(sub(".JPG*", "", sub("DSCN", "", name))))
+    mutate(photo_number = ifelse(test = grepl(pattern = Location, name),
+                                 yes = 0,
+                                 no = NA))
+  
+  a <- a %>% 
+    mutate(photo_number = ifelse(test = grepl(pattern = "DSCN", name),
+                                 yes = as.numeric(sub(".JPG*", "", sub("DSCN", "", name))),
+                                 no = photo_number))
+  
+  a <- a %>% 
+    mutate(photo_number = ifelse(test = grepl(pattern = "IMG", name),
+                                 yes = as.numeric(sub(".JPG*", "", sub("IMG_", "", name))),
+                                 no = photo_number))
   
   a <- a %>% 
     mutate(photo_number = ifelse(test = is.na(photo_number),
                                  yes = tools::file_path_sans_ext(name),
                                  no = photo_number))
   
-  a <- a %>% 
-    mutate(photo_number = ifelse(test = is.na(photo_number),
-                                 yes = as.numeric(sub(".JPG*", "", sub("IMG_", "", name))),
-                                 no = photo_number))
-  
+
   
   moth_id_df <- moth_id_dataframe %>% 
     mutate(photoStart_number = as.numeric(sub(".JPG*", "", sub("DSCN", "", photoStart)))) %>% 
     mutate(photoEnd_number = as.numeric(sub(".JPG*", "", sub("DSCN", "", photoEnd)))) %>% 
+    mutate(num_of_photos = photoEnd_number - photoStart_number + 1)
+  
+  moth_id_df <- moth_id_df %>% 
+    mutate(photoStart_number = ifelse(test = is.na(photoStart_number),
+                                      yes = as.numeric(sub(".JPG*", "", sub("IMG_", "", photoStart))),
+                                      no = photoStart_number)) %>% 
+    mutate(photoEnd_number = ifelse(test = is.na(photoEnd_number),
+                                    yes = as.numeric(sub(".JPG*", "", sub("IMG_", "", photoEnd))),
+                                    no = photoEnd_number)) %>% 
     mutate(num_of_photos = photoEnd_number - photoStart_number + 1)
   
   for(i in 1:nrow(a)){
