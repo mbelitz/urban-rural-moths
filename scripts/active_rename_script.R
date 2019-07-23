983fdf5477d168df5eacdaba988332e71ff6af9d
Michael Belitz <michaelbelitz06@gmail.com>
2019-07-23 13:52:51 -0400

    updated by adding third parameter

--- scripts/active_rename_script.R
+++ scripts/active_rename_script.R
@@ -3,15 +3,39 @@ source(file = "scripts/rename_photos_function.R") # Funciton takes two parameter
 
 # 1. drive_ls_path
 # 2. moth_id_dataframe
+# 3. Location where moth was sampled -- must be in ""
 
 # Rename RIST Photos
 
 moth_id <- gs_title(x = "Moth Identification")
 df <- gs_read(moth_id)
 
-rist <- df %>% 
+RIST <- df %>% 
   dplyr::filter(Location == "RIST")
 
 rename_photos(drive_ls_path = "Moth Phenology/Identification Photos/RIST",
-              moth_id_dataframe = rist)
+              moth_id_dataframe = RIST, Location = "RIST")
+
+
+## RENAME BACA PHOTOS
+
+moth_id <- gs_title(x = "Moth Identification")
+df <- gs_read(moth_id)
+
+BACA <- df %>% 
+  dplyr::filter(Location == "BACA")
+
+rename_photos(drive_ls_path = "Moth Phenology/Identification Photos/BACA",
+              moth_id_dataframe = BACA, Location = "BACA")
+
+## RENAME PRCR Photos
+
+moth_id <- gs_title(x = "Moth Identification")
+df <- gs_read(moth_id)
+
+PRCR <- df %>% 
+  dplyr::filter(Location == "PRCR")
+
+rename_photos(drive_ls_path = "Moth Phenology/Identification Photos/PRCR",
+              moth_id_dataframe = PRCR, Location = "PRCR")
 
