3352e86d4f52e10a489e68b8077419241207af4c
Michael Belitz <michaelbelitz06@gmail.com>
2019-08-20 14:40:20 -0400

    works, but need to make a function for this

--- scripts/add_identifications_to_googlesheet.R
+++ scripts/add_identifications_to_googlesheet.R
@@ -4,11 +4,10 @@ library(readr)
 library(dplyr)
 
 # Read in Google Sheet to R
-moth_id <- gs_title(x = "Moth Identification")
+moth_id <- gs_title(x = "Raw Moth Photo Numbers")
 df <- gs_read(moth_id)
 
 AUCA_dirs <- drive_ls("Moth Phenology/Identification Photos/AUCA", type = "folder")
-
 AUCA_Notodontidae_files <- drive_ls("Moth Phenology/Identification Photos/AUCA/Notodontidae")
 
 df_parsenum <- df %>% 
@@ -18,25 +17,22 @@ df_parsenum <- df %>%
 df_parsenum$photoStartNum = as.numeric(df_parsenum$photoStartNum)
 df_parsenum$photoEndNum = as.numeric(df_parsenum$photoEndNum)
 
+df_parsenum <- df_parsenum %>% 
+  mutate(name = paste(Location, eventDate, photoStartNum, sep = "_"))
+
 AUCA_Notodontidae_files <- AUCA_Notodontidae_files %>% 
   mutate(photoNum = as.numeric(substring(AUCA_Notodontidae_files$name, first = 17))) %>% 
   mutate(Family = "Notodontidae")
 
-# Write new df
+AUCA_Notodontidae_join <- left_join(AUCA_Notodontidae_files, df_parsenum, by = "name")
 
-for(i in 1:length(AUCA_Notodontidae_files)){
-  testdf <- df_parsenum %>% 
-    mutate(Family = ifelse(AUCA_Notodontidae_files$photoNum[i] >= photoStartNum & 
-                         AUCA_Notodontidae_files$photoNum[i] <= photoEndNum, 
-                         yes = AUCA_Notodontidae_files$Family[i],
-                         no = Family))
-  
-}
+AUCA_Notodontidae_unique <- dplyr::filter(AUCA_Notodontidae_join, !is.na(photoStart)) %>% 
+  dplyr::select(-name, -id, -drive_resource, - photoNum)
 
-testdf <- mutate(df_parsenum, test = photoStartNum <= AUCA_Notodontidae_files$photoNum & 
-        photoEndNum >= AUCA_Notodontidae_files$photoNum)
+df_id <- left_join(df, AUCA_Notodontidae_unique)
 
-which(AUCA_Notodontidae_files$photoNum %in% 
-      
-      >= df_parsenum$photoStartNum & 
-  AUCA_Notodontidae_files$photoNum <= df_parsenum$photoEndNum)
+write.csv(df_id, "outputs/id_dataframe.csv")
+
+gs_upload(file = "outputs/id_dataframe.csv", sheet_title = "Identified Moth Photos", overwrite = TRUE)
+
+drive_mv(file = "Identified Moth Photos", path = "~/Moth Phenology/")
