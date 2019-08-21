d37b1aed4e4dd684a73b5084e48f7e3e32bb017c
Michael Belitz <michaelbelitz06@gmail.com>
2019-08-21 09:45:28 -0400

    function to move ID'd photos

--- scripts/move_photos_to_new_folders.R
+++ scripts/move_photos_to_new_folders.R
@@ -1,18 +1,35 @@
 library(googledrive)
 
-#Use this when moving photos to Erebidae folder 
-erebidae <- as_dribble("Moth Phenology/Identification Photos/AUCA/Erebidae")
+# Function to move Moth Photos in Google Drive
 
-drive_mv(file = "AUCA_2019-04-21_6599", path = erebidae)
+move_photos <- function(id_folder, site_folder, date, photo_start_num, photo_end_num){
+  folder_path <- as_dribble(paste("Moth Phenology/Identification Photos", site_folder, id_folder, sep = "/"))
+  
+  
+  for(i in photo_start_num:photo_end_num){
+    drive_mv(file = paste(site_folder, date, i, sep = "_"), path = folder_path)
+    
+  }
+}
+
+# Examples
+
+move_photos(id_folder = "Erebidae", site_folder = "RIST", 
+            date = "2019-04-07", photo_start_num = 1424, photo_end_num = 1427)
+
+
+
+
+
 
 # Use this part when moving photos to Notodontidae folder
-
-
 
 for(i in 6833:6834){
   drive_mv(file = paste("AUCA_2019-03-16_", i, sep = ""), path = erebidae)
   
 }
+
+erebidae <- as_dribble("Moth Phenology/Identification Photos/RIST/Erebidae")
 
 
 
