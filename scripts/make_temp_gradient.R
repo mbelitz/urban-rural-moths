eac69bfb2e3b57fb9eb7f72f501b567077ad83ca
Michael Belitz <michaelbelitz06@gmail.com>
2020-11-10 13:44:38 -0500

    added mean calculations

--- scripts/make_temp_gradient.R
+++ scripts/make_temp_gradient.R
@@ -17,7 +17,9 @@ ggplot( ) +
 
 
 jdf <- left_join(sphinx, tiger, by = "FORMATTED.DATE.TIME") %>% 
-  mutate(diff = Temperature.x - Temperature.y)
+  mutate(temp_diff = Temperature.x - Temperature.y,
+         rh_diff = Relative.Humidity.x - Relative.Humidity.y,
+         hsi_diff = Heat.Stress.Index.x - Heat.Stress.Index.y)
 
 ggplot(jdf) + 
   geom_point(mapping = aes(x = doy.x, y = Temperature.x, color = SITE.x)) +
@@ -26,4 +28,83 @@ ggplot(jdf) +
   geom_smooth(mapping = aes(x = doy.y, y = Temperature.y, color = "not BACA"))
 
 ggplot(jdf) + 
-  geom_boxplot(mapping = aes(x = SITE.y, y = diff))
+  geom_boxplot(mapping = aes(x = SITE.y, y = temp_diff))
+
+jdf2 <- na.omit(jdf) %>% 
+  filter(SITE.y != "")
+
+ggplot(jdf2) + 
+  geom_boxplot(mapping = aes(x = SITE.y, y = temp_diff))
+
+gradient <- jdf2 %>% 
+  group_by(SITE.y) %>% 
+  summarise(mean_temp = mean(temp_diff),
+            mean_rh = mean(rh_diff),
+            mean_hsi = mean(hsi_diff))
+
+
+
+
+
+
+
+
+
+
+
+
+
+auca <- filter(jdf2, SITE.y == "AUCA")
+
+ggplot(auca) +   
+  geom_point(mapping = aes(x = FORMATTED.DATE.TIME, y = Temperature.y, color = SITE.y)) 
+
+prcr <- filter(jdf2, SITE.y == "PRCR")
+
+ggplot(prcr) +   
+  geom_point(mapping = aes(x = FORMATTED.DATE.TIME, y = Temperature.y, color = SITE.y)) 
+
+rist <- filter(jdf2, SITE.y == "RIST")
+
+ggplot(rist) +   
+  geom_point(mapping = aes(x = FORMATTED.DATE.TIME, y = Temperature.y, color = SITE.y)) 
+
+
+demi <- filter(jdf2, SITE.y == "DEMI")
+
+ggplot(demi) +   
+  geom_point(mapping = aes(x = FORMATTED.DATE.TIME, y = Temperature.y, color = SITE.y)) 
+
+bowa <- filter(jdf2, SITE.y == "BOWA")
+
+ggplot(bowa) +   
+  geom_point(mapping = aes(x = FORMATTED.DATE.TIME, y = Temperature.y, color = SITE.y)) 
+
+
+biva <- filter(jdf2, SITE.y == "BIVA")
+
+ggplot(biva) +   
+  geom_point(mapping = aes(x = FORMATTED.DATE.TIME, y = Temperature.y, color = SITE.y)) 
+
+
+cofr <- filter(jdf2, SITE.y == "COFR")
+
+ggplot(cofr) +   
+  geom_point(mapping = aes(x = FORMATTED.DATE.TIME, y = Temperature.y, color = SITE.y)) 
+
+
+
+joma <- filter(jdf2, SITE.y == "JOMA")
+
+ggplot(joma) +   
+  geom_point(mapping = aes(x = FORMATTED.DATE.TIME, y = Temperature.y, color = SITE.y)) 
+
+baca <- filter(jdf2, SITE.x == "BACA")
+
+ggplot(baca) +   
+  geom_point(mapping = aes(x = FORMATTED.DATE.TIME, y = Temperature.x, color = SITE.x)) 
+
+baca2 <- filter(baca, doy.x >= 275 & doy.x <= 290)
+
+ggplot(baca2) +   
+  geom_point(mapping = aes(x = FORMATTED.DATE.TIME, y = Temperature.x, color = SITE.x)) 
