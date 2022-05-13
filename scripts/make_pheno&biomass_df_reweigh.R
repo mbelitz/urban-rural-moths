eacc54572b64570dab4589969fa77d38bed539c6
mbelitz <mbelitz@ufl.edu>
2022-05-13 16:34:17 -0400

    add plotting scripts

--- scripts/make_pheno&biomass_df_reweigh.R
+++ scripts/make_pheno&biomass_df_reweigh.R
@@ -31,7 +31,7 @@ frass_group <-  frass %>%
 
 ggplot(frass_group, mapping = aes(x = doy2, y = avg_mass, color = Site)) +
   geom_point(alpha = 0.2) + 
-  geom_smooth(se = F, formula = y ~ s(x, bs = "cc"), method = "gam") + 
+  geom_smooth(se = F, formula = y ~ s(x), method = "gam") + 
   theme_classic() +
   facet_wrap(~urban)
 
@@ -41,9 +41,9 @@ ggplot(frass_group, mapping = aes(x = doy2, y = avg_mass, color = Site)) +
 site_gam <- function(site){
   
   mdf <- filter(frass_group, Site == site)
-  sgam <- gam(avg_mass ~ s(doy2, bs = "cc"), family = gaussian, gamma = 1, data = mdf)
+  sgam <- gam(avg_mass ~ s(doy2), family = gaussian, gamma = 1, data = mdf)
   
-  all_doys <-  data.frame(doy2 = 1:365)
+  all_doys <-  data.frame(doy2 = 62:424)
   all_doys$avg_mass <-  predict(sgam, newdata = all_doys, 
                                 type = 'response')
   
@@ -96,10 +96,12 @@ total_gam <- rbind(joma_gam, baca_gam, cofr_gam,
 
 
 ggplot(total_gam, mapping = aes(x = doy2, y = avg_mass, color = Site)) +
-  geom_smooth(se = F, formula = y ~ s(x, bs = "cc"), method = "gam") + 
+  geom_point(frass_group, mapping = aes(x = doy2, y = avg_mass, color = Site)) +
+  geom_smooth(se = F, formula = y ~ s(x), method = "gam") + 
   theme_classic() +
   facet_wrap(~Class)
 
+ggsave("outputs/frass_gams.png", height = 4, width = 6)
 
 ## MAKE PHENOLOGY ESTIMATES
 #' function to make phenoestimates from gam
@@ -159,7 +161,7 @@ frass_pheno_df <- data.frame(
   
 )
 
-write.csv(frass_pheno_df, "outputs/frass_pheno_reweigh.csv", row.names = F)
+#write.csv(frass_pheno_df, "outputs/frass_pheno_reweigh.csv", row.names = F)
 
 ## MAKE BIOMASS Estimates
 frass_biomass_df <- data.frame(
@@ -184,4 +186,65 @@ frass_biomass_df <- data.frame(
   
 )
 
-write.csv(frass_biomass_df, "outputs/frass_biomass_reweigh.csv", row.names = F)
+#write.csv(frass_biomass_df, "outputs/frass_biomass_reweigh.csv", row.names = F)
+
+
+# First we will do this for the tenth percentile
+
+#Let's visualize the data first
+pheno_data <- frass_pheno_df
+
+ggplot() + 
+  geom_boxplot(pheno_data, mapping = aes(x = Class, y = tenth))
+
+tenth.anova <- aov(tenth ~ Class, data = pheno_data)
+summary(tenth.anova)
+TukeyHSD(tenth.anova)
+
+ggplot() + 
+  geom_boxplot(pheno_data, mapping = aes(x = Class, y = fifty))
+
+fifty.anova <- aov(fifty ~ Class, data = pheno_data)
+summary(fifty.anova)
+TukeyHSD(fifty.anova)
+
+ggplot() + 
+  geom_boxplot(pheno_data, mapping = aes(x = Class, y = ninty))
+
+ninty.anova <- aov(ninty ~ Class, data = pheno_data)
+summary(ninty.anova)
+TukeyHSD(ninty.anova)
+
+# Now let's add in the Urbanization gradient from GIS DATA
+urb_data <- read.csv("data_products/urbanization_gradient.csv")
+pheno_data_urb <- left_join(pheno_data, urb_data, by = "Site")
+
+ggplot(pheno_data_urb, aes(x = Dev_10, y = tenth)) + 
+  geom_point(mapping = aes(color = Class)) + 
+  geom_smooth(mapping = aes(), method = 'lm')
+
+tenth.lm <- lm(formula = tenth ~ Dev_10, data = pheno_data_urb)
+summary(tenth.lm)  
+
+ggplot(pheno_data_urb, aes(x = Dev_10, y = fifty)) + 
+  geom_point(mapping = aes(color = Class)) + 
+  geom_smooth(mapping = aes(), method = 'lm') + 
+  labs(x = "Proportion Developed 100km^2", y = "DOY of 50th Percentile") +
+  theme_classic()
+
+fifty.lm <- lm(formula = fifty ~ Dev_10, data = pheno_data_urb)
+summary(fifty.lm)  
+
+ggplot(pheno_data_urb, aes(x = Dev_10, y = ninty)) + 
+  geom_point(mapping = aes(color = Class)) + 
+  geom_smooth(mapping = aes(), method = 'lm')
+
+ninty.lm <- lm(formula = ninty ~ Dev_10, data = pheno_data_urb)
+summary(ninty.lm)  
+
+## Try to see if you can do this on your own for the fiftieth and nintieth percentiles
+## FOr both annovas and lm 
+## What do these results mean?
+
+## Also try the whole process for biomass (you'll have to read in new data)
+
